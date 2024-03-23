b0595bf796924e66e39f157f571f65306e507732
DumakIt <odumak4411@gmail.com>
2024-03-23 21:12:58 +0900

    Feet: Third Person Camera 구현

--- src/app/_components/character.tsx
+++ src/app/_components/character.tsx
@@ -1,9 +1,27 @@
+import * as THREE from "three";
 import { useGLTF } from "@react-three/drei";
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useRef } from "react";
+import { useCharacterCam } from "../_hooks/useCharacterCam";
+import { useThree } from "@react-three/fiber";
 
 export default function Character() {
+  const groupRef = useRef<THREE.Group>(null);
+  const { camera } = useThree();
   const { scene } = useGLTF("/models/femaleTypeA.glb");
+  const { XRotation, YRotation } = useCharacterCam();
+
+  useLayoutEffect(() => {
+    if (camera && groupRef.current) {
+      // 카메라 관련 기본 설정
+      camera.position.set(scene.position.x, scene.position.y + 2, scene.position.z - 2.5); // 카메라 기본 위치 설정
+      camera.lookAt(scene.position.x, scene.position.y + 1, scene.position.z); // 카메라가 캐릭터를 바라보게 설정
+
+      groupRef.current?.add(YRotation); // YRotation을 groupRef 그룹에 추가
+      YRotation.add(XRotation); // XRotation을 YRotation 그룹에 추가
+      XRotation.add(camera); // 카메라를 XRotation 그룹에 추가
+    }
+  }, [camera, groupRef, XRotation, YRotation, scene]);
 
   useLayoutEffect(() => {
     if (scene) {
@@ -18,7 +36,9 @@ export default function Character() {
   return (
     <RigidBody colliders={false} position={[0, 1.6, 0]} friction={2} lockRotations>
       <CapsuleCollider args={[0.4, 0.4]} position={[0, 0.8, 0]} />
-      <primitive object={scene} />
+      <group ref={groupRef}>
+        <primitive object={scene} />
+      </group>
     </RigidBody>
   );
 }
---
+++ src/app/_hooks/useCharacterCam.ts
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import { useLayoutEffect, useMemo } from "react";
+
+export const useCharacterCam = () => {
+  const XRotation = useMemo(() => new THREE.Group(), []); // 카메라 상하(X축) 회전 값 저장
+  const YRotation = useMemo(() => new THREE.Group(), []); // 카메라 좌우(Y축) 회전 값 저장
+
+  useLayoutEffect(() => {
+    if (XRotation && YRotation) {
+      const handleMouseMove = (event: MouseEvent) => {
+        // mousemove 이벤트는 마우스의 이동값을 상하가 movementY, 좌우가 movementX 이며
+        // 마우스를 위로 올리거나, 우측으로 이동시 "+"값 이기 때문에 "-"를 붙임
+        YRotation.rotateY(-event.movementX * 0.003);
+
+        // currentX는 현재 상하 값을 나타냄 일정 값 이상 회전 못하게 막기위해 사용
+        const currentX = XRotation.rotation.x - event.movementY * 0.003;
+        if (currentX > -0.4 && currentX < 0.5) {
+          XRotation.rotateX(-event.movementY * 0.003);
+        }
+      };
+
+      document.addEventListener("mousemove", handleMouseMove);
+
+      return () => {
+        document.removeEventListener("mousemove", handleMouseMove);
+      };
+    }
+  }, [XRotation, YRotation]);
+
+  return { XRotation, YRotation };
+};
