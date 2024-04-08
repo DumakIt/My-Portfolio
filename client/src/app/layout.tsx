3b27ea84afe034fffa49dca8790d8d6550938af7
DumakIt <odumak4411@gmail.com>
2024-04-08 23:27:20 +0900

    Rename: globals.css 및 RootLayout 위치 이동

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import RecoilSetting from "../_lib/recoilSetting";
+import RecoilSetting from "./_lib/recoilSetting";
 
 export const metadata: Metadata = {
   title: "Create Next App",
