9eabd7e27f63c0733aae645ee1ce8bb16aecf44f
DumakIt <odumak4411@gmail.com>
2024-04-29 21:43:46 +0900

    Refactor: KeyInfo Image에 priority 속성 추가

--- client/src/app/(route)/_components/keyInfo.tsx
+++ client/src/app/(route)/_components/keyInfo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function KeyInfo() {
   return (
     <section className={style.container}>
-      <Image src="/images/keyInfo.svg" alt="조작방법 이미지" width={350} height={140} />
+      <Image src="/images/keyInfo.svg" alt="조작방법 이미지" width={350} height={140} priority />
     </section>
   );
 }
